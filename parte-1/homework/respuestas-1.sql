2afffbf8fdab60edd1cc795410ab10c91d1625fd
Eduardo Montesinos Cabrero <132164261+EduardoMontesinos@users.noreply.github.com>
2023-12-01 15:01:45 +0100

    Update respuestas-1.sql

--- parte-1/homework/respuestas-1.sql
+++ parte-1/homework/respuestas-1.sql
@@ -2,74 +2,173 @@
 
 
 -- 1. Mostrar todos los productos dentro de la categoria electro junto con todos los detalles.
-select * from stg.product_master where categoria = 'Electro'
+select * from stg.product_master where category = 'Electro'
 
 -- 2. Cuales son los producto producidos en China?
+select name, origin from stg.product_master where origin= 'China'
 
 -- 3. Mostrar todos los productos de Electro ordenados por nombre.
+select name, category from stg.product_master where category = 'Electro' order by 1 asc
 
 -- 4. Cuales son las TV que se encuentran activas para la venta?
+select * from stg.product_master where subcategory = 'TV' and is_active = 'true'
 
 -- 5. Mostrar todas las tiendas de Argentina ordenadas por fecha de apertura de las mas antigua a la mas nueva.
+select * from stg.store_master where country = 'Argentina' order by start_date asc
 
 -- 6. Cuales fueron las ultimas 5 ordenes de ventas?
+select * from stg.order_line_sale order by date desc limit 5
 
 -- 7. Mostrar los primeros 10 registros de el conteo de trafico por Super store ordenados por fecha.
+select * from stg.super_store_count order by date desc limit 10
 
 -- 8. Cuales son los producto de electro que no son Soporte de TV ni control remoto.
+select * from stg.product_master where category ='Electro' and subsubcategory not in ('TV','Control remoto')
 
 -- 9. Mostrar todas las lineas de venta donde el monto sea mayor a $100.000 solo para transacciones en pesos.
+select * from stg.order_line_sale where sale > 100000 and currency ='ARS'
 
 -- 10. Mostrar todas las lineas de ventas de Octubre 2022.
+select * from stg.order_line_sale where cast(date as varchar) like '2022-10-%'
+--between '2022-10-01' and '2022-10-31'
 
 -- 11. Mostrar todos los productos que tengan EAN.
+select * from stg.product_master where ean is not null
 
 -- 12. Mostrar todas las lineas de venta que que hayan sido vendidas entre 1 de Octubre de 2022 y 10 de Noviembre de 2022.
-
+select * from stg.order_line_sale where date between '2022-10-01' and '2022-11-10'
 
 -- ## Semana 1 - Parte B
 
 -- 1. Cuales son los paises donde la empresa tiene tiendas?
+select distinct country from stg.store_master
 
 -- 2. Cuantos productos por subcategoria tiene disponible para la venta?
+select subcategory, count(subcategory) from stg.product_master 
+where is_active = 'true' group by subcategory
 
 -- 3. Cuales son las ordenes de venta de Argentina de mayor a $100.000?
-
+select * 
+from stg.order_line_sale a
+left join stg.store_master b
+on a.store=b.store_id
+where sale > 100000
 -- 4. Obtener los decuentos otorgados durante Noviembre de 2022 en cada una de las monedas?
+select currency, sum(promotion) as total_promotion
+from stg.order_line_sale 
+where cast(date as varchar) like '2022-11-%'
+group by currency
 
 -- 5. Obtener los impuestos pagados en Europa durante el 2022.
+select currency, sum(tax) as total_tax
+from stg.order_line_sale 
+where cast(date as varchar) like '2022-%' and currency = 'EUR'
+group by currency
 
 -- 6. En cuantas ordenes se utilizaron creditos?
+select count(*) from stg.order_line_sale where credit is not null
 
--- 7. Cual es el % de descuentos otorgados (sobre las ventas) por tienda?
+-- 7. Cual es el % de descuentos otorgados (sobre las ventas) por tienda?	
+select distinct store, round(((sum(promotion) / sum(sale)) * 100),2) as Promotion
+from stg.order_line_sale
+group by store
 
 -- 8. Cual es el inventario promedio por dia que tiene cada tienda?
+SELECT store_id, date, round(avg(initial+final),2) as avg_inventory
+from stg.inventory
+group by store_id, date
 
 -- 9. Obtener las ventas netas y el porcentaje de descuento otorgado por producto en Argentina.
+select distinct
+	product
+	, round(coalesce((sale-promotion-tax),sale),2) as ventas_netas
+	, coalesce(round(((promotion/sale)*100),2),0) as descuento_venta
+from stg.order_line_sale
+where currency ='ARS'
 
--- 10. Las tablas "market_count" y "super_store_count" representan dos sistemas distintos que usa la empresa para contar la cantidad de gente que ingresa a tienda, uno para las tiendas de Latinoamerica y otro para Europa. Obtener en una unica tabla, las entradas a tienda de ambos sistemas.
+-- 10. Las tablas "market_count" y "super_store_count" representan dos sistemas distintos que usa la empresa para contar la cantidad de gente que ingresa a tienda,
+--uno para las tiendas de Latinoamerica y otro para Europa. Obtener en una unica tabla, las entradas a tienda de ambos sistemas.
+select store_id, to_char(to_date(date::text, 'YYYYMMDD'),'YYYY-MM-DD') as date, traffic
+from stg.market_count
+union all
+select * from stg.super_store_count
+
 
 -- 11. Cuales son los productos disponibles para la venta (activos) de la marca Phillips?
+select name from stg.product_master where is_active = 'true' and name like '%PHILIPS%'
 
 -- 12. Obtener el monto vendido por tienda y moneda y ordenarlo de mayor a menor por valor nominal de las ventas (sin importar la moneda).
+select store, currency, sum(sale)
+from stg.order_line_sale
+group by store, currency
+order by sum(sale) desc
 
 -- 13. Cual es el precio promedio de venta de cada producto en las distintas monedas? Recorda que los valores de venta, impuesto, descuentos y creditos es por el total de la linea.
+select product, currency, round(avg(sale),2) as precio_promedio
+from stg.order_line_sale
+group by product, currency
+order by 1
 
 -- 14. Cual es la tasa de impuestos que se pago por cada orden de venta?
-
+select order_number, coalesce(round(((tax/sale) * 100),2),0) as tasa_impuestos
+from stg.order_line_sale
+group by order_number, tax, sale
 
 -- ## Semana 2 - Parte A
 
 -- 1. Mostrar nombre y codigo de producto, categoria y color para todos los productos de la marca Philips y Samsung, mostrando la leyenda "Unknown" cuando no hay un color disponible
+select 
+	name
+	, product_code
+	, category
+	, coalesce(color, 'Unknown') as color
+from stg.product_master
+where name like '%PHILIPS%' or name like '%SAMSUNG%'
 
 -- 2. Calcular las ventas brutas y los impuestos pagados por pais y provincia en la moneda correspondiente.
+select distinct
+	b.country
+	, b.province
+	, a.currency
+	, round(sum(a.sale),2) as ventas_brutas
+	, round(sum(a.tax),2) as total_impuestos
+from stg.order_line_sale a
+left join stg.store_master b
+on a.store=b.store_id
+group by 1, 2, 3
 
 -- 3. Calcular las ventas totales por subcategoria de producto para cada moneda ordenados por subcategoria y moneda.
-  
--- 4. Calcular las unidades vendidas por subcategoria de producto y la concatenacion de pais, provincia; usar guion como separador y usarla para ordernar el resultado.
+select distinct
+	b.subcategory
+	, a.currency
+	, round(sum(a.sale),2) as ventas_totales
+from stg.order_line_sale a
+left join stg.product_master b
+on a.product=b.product_code
+group by 1,2
+order by 1,2
+
+-- 4. Calcular las unidades vendidas por subcategoria de producto y la concatenacion de pais, provincia; 
+--usar guion como separador y usarla para ordernar el resultado.
+select
+	b.subcategory
+	,sum(a.quantity) as unidades_vendidas
+	, concat(c.country,'-',c.province) as pais_provincia
+from stg.order_line_sale a
+left join stg.product_master b on a.product=b.product_code
+left join stg.store_master c on a.store=c.store_id
+group by 1, 3
+order by 3
   
 -- 5. Mostrar una vista donde sea vea el nombre de tienda y la cantidad de entradas de personas que hubo desde la fecha de apertura para el sistema "super_store".
-  
+select 
+	b.name
+	, sum(traffic) as cantidad_entradas
+from stg.super_store_count a
+left join stg.store_master b on a.store_id=b.store_id
+where cast(a.date as date) >= b.start_date
+group by a.store_id, b.name
+ 
 -- 6. Cual es el nivel de inventario promedio en cada mes a nivel de codigo de producto y tienda; mostrar el resultado con el nombre de la tienda.
   
 -- 7. Calcular la cantidad de unidades vendidas por material. Para los productos que no tengan material usar 'Unknown', homogeneizar los textos si es necesario.
